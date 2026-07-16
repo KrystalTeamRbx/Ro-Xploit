2fd2d98338af24c9c8834da7bb09b4b1ad16d09f
Krystal Team <wiiblox.com@gmail.com>
2026-07-15 23:15:40 -0300

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-https://raw.githubusercontent.com/KrystalTeamRbx/Ro-Xploit/refs/heads/main/source/ro-xploit.lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KrystalTeamRbx/Ro-Xploit/refs/heads/main/source/ro-xploit.lua"))()
